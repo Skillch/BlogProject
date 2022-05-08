6cb412521cc1d65465340f2fb6d874a6a4e599af
Skillch <96367855+Skillch@users.noreply.github.com>
2022-05-08 13:12:29 +0300

    Start

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./index.css";
+
+const Footer = () => {
+    return(
+        <footer>
+            <div>
+               
+                <p>&copy;2022</p>
+            </div>
+            <ul>
+                <li><a href="">link</a></li>
+                <li><a href="">link</a></li>
+                <li><a href="">link</a></li>
+                <li><a href="">link</a></li>
+            </ul>
+            <ul>
+                <li><a href="">link</a></li>
+                <li><a href="">link</a></li>
+                <li><a href="">link</a></li>
+                <li><a href="">link</a></li>
+            </ul>
+            <div>
+                <h4>Что то</h4>
+                <p>telephon</p>
+                <p>Email</p>
+                {/* <ul className="soc">
+                    <li><a href="">soc</a></li>
+                    <li><a href="">soc</a></li>
+                    <li><a href="">soc</a></li>
+                    <li><a href="">soc</a></li>
+                    <li><a href="">soc</a></li>
+                </ul> */}
+            </div>
+            
+        </footer>
+    )
+}
+export default Footer
