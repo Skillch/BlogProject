3976b799df62cd59148b3c0e65a79c14de2335c2
vickhod <vickhodulova@gmail.com>
2022-05-12 15:55:30 +0900

    Добавила стиль текущей страницы

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./index.css";
 
-const Pagination = ({postsPerPage, totalPosts, paginate}) => {
+const Pagination = ({currentPage, postsPerPage, totalPosts, paginate}) => {
     const pageNumbers = [];
 
     for (let i=1; i<= Math.ceil(totalPosts/postsPerPage); i++) {
@@ -14,8 +14,8 @@ const Pagination = ({postsPerPage, totalPosts, paginate}) => {
             <ul className="pagination">
                 {
                     pageNumbers.map(number => (
-                        <li className="page-item" key={number}>
-                            <Link to={number} className="page-link" onClick={() => paginate(number)}>
+                        <li className="page__item" key={number}>
+                            <Link to={number} className={currentPage === number ? "page__current page__link" : "page__link"}  onClick={() => paginate(number)}>
                                 {number}
                             </Link>
                         </li>
