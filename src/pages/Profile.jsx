acd0220c137dd75ef6e596e419f82ca4a05ab41c
VicKhod <vickhodulova@gmail.com>
2022-05-14 15:01:42 +0900

    Profila

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,7 @@
-import React, {useContext} from "react";
-import { useNavigate } from "react-router-dom";
+import React, {useState, useContext, useEffect} from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { UserCtx } from "../components/Context/UserContext";
+import api from "../Api";
 import "../index.css";
 
 export default () => {
@@ -17,8 +18,15 @@ export default () => {
 
     return ( 
         <div className="profile">
-            <h1>Личный кабинет</h1>
-            <span className="logout" onClick={logout}>Выйти</span>
+            <div className="user__btn">
+                <h1>Личный кабинет</h1>
+                <span className="logout" onClick={logout}>Выйти</span>
+            </div>
+            
+            <div className="user__card">
+                <h2>Добро пожаловать</h2>
+                <img src="https://img5.goodfon.ru/wallpaper/nbig/6/ba/romashki-nebo-fon.jpg" alt="Добро пожаловать!" />
+            </div>
         </div>
     )
 }
